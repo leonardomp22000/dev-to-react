339ed8e1a4b4cc7643900cdf997d18e8ecaf2772
Leonardo <leonardomp2200@gmail.com>
2024-07-10 21:54:31 -0600

    feat: Valildations of Form registration

--- pages/CreateUserForm.jsx
+++ pages/CreateUserForm.jsx
@@ -1,27 +1,30 @@
 import { useForm } from "react-hook-form";
 import { registration } from "@/api";
-
+import clsx from "clsx";
 
 export default function CreateUserForm() {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
   async function onSubmit(data) {
- 
     try {
       const response = await registration(data.name, data.email, data.password);
-      
+
       if (response) {
         // Apartados para sonner
         console.log("Registrado correctamente");
-      }else{
+      } else {
         // Apartado para sonner
-        console.log("Las credenciales ya estan en uso")
+        console.log("Las credenciales ya estan en uso");
       }
-      
     } catch (error) {
-      console.error("Error al registrar: ", error)
+      console.error("Error al registrar: ", error);
     }
- 
   }
+  const password = watch("password");
   return (
     <main className="bg-white min-h-screen p-12 ">
       <section className="flex justify-center">
@@ -36,9 +39,16 @@ export default function CreateUserForm() {
             <div className="flex flex-col gap-2">
               <label htmlFor="name">Name</label>
               <input
-                className="w-full p-2 border border-[color:var(--border-color-input)] rounded-lg"
+                className={clsx(
+                  "w-full p-2 border border-[color:var(--border-color-input)] rounded-lg",
+                  { "border border-red-600 bg-red-100": errors.name }
+                )}
                 type="text"
                 {...register("name", {
+                  minLength: {
+                    value: 3,
+                    message: "El nombre debe tener al menos 3 letras",
+                  },
                   required: {
                     value: true,
                     message: "El campo es requerido",
@@ -46,27 +56,51 @@ export default function CreateUserForm() {
                 })}
               />
             </div>
+            {errors.name && (
+              <p className="text-red-600">{errors.name?.message}</p>
+            )}
 
             <div className="flex flex-col gap-2">
               <label htmlFor="email">Email</label>
               <input
-                className="w-full p-2 border border-[color:var(--border-color-input)] rounded-lg"
+
+                className={clsx("w-full p-2 border border-[color:var(--border-color-input)] rounded-lg", {"border border-red-600 bg-red-100": errors.email})}
                 type="email"
                 {...register("email", {
                   required: {
                     value: true,
                     message: "El campo es requerido",
                   },
+                  pattern: {
+                    value: /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/, 
+                    message: "No es un correo valido"
+                  }
                 })}
               />
             </div>
+            {
+              errors.email && <p className="text-red-600">{errors.email?.message}</p>
+            }
 
+           
             <div className="flex flex-col gap-2">
               <label htmlFor="password">Password</label>
               <input
-                className="w-full p-2 border border-[color:var(--border-color-input)] rounded-lg"
-                type="text"
+                className={clsx(
+                  "w-full p-2 border border-[color:var(--border-color-input)] rounded-lg",
+                  { "border border-red-600 bg-red-100": errors.password }
+                )}
+                type="password"
                 {...register("password", {
+                  minLength: {
+                    value: 6,
+                    message: "La contraseña debe ser de al menos 6 caracteres",
+                  },
+
+                  maxLength:{
+                    value: 20, 
+                    message: "La contraseña debe tener menos de 20 caracteres"
+                  },
                   required: {
                     value: true,
                     message: "El campo es requerido",
@@ -75,14 +109,48 @@ export default function CreateUserForm() {
               />
             </div>
 
+            {errors.password && (
+              <p className="text-red-600">{errors.password?.message}</p>
+            )}
+
             <div className="flex flex-col gap-2">
-              <label htmlFor="password-confirmation">
+              <label htmlFor="passwordConfirmation">
                 Password Confirmation
               </label>
               <input
-                className="w-full p-2 border border-[color:var(--border-color-input)] rounded-lg"
-                type="text"
-                {...register("pasword-confirmation", {
+                className={clsx(
+                  "w-full p-2 border border-[color:var(--border-color-input)] rounded-lg",
+                  {
+                    "border border-red-600 bg-red-100":
+                      errors.passwordConfirmation,
+                  }
+                )}
+                /* DIFERENTES FORMAS DE ESCRIBIR EL CALLBACK DE VALIDACION
+
+                value => {
+                    return value === password || "The passwords do not match";
+                }
+
+
+                value => value === password ? true : "The passwords do not match";
+
+
+                value => {
+                    if (value === password) {
+                          return true;
+                    } else {
+                          return "The passwords do not match";
+                    }
+                }
+
+                value => value === password || undefined ----> Este ultimo es una mala practica para la lectura del codigo
+                
+                */
+                type="password"
+                {...register("passwordConfirmation", {
+                  validate: (value) =>
+                    value === password || "El password no coincide",
+
                   required: {
                     value: true,
                     message: "El campo es requerido",
@@ -90,6 +158,12 @@ export default function CreateUserForm() {
                 })}
               />
             </div>
+
+            {errors.passwordConfirmation && (
+              <p className="text-red-600">
+                {errors.passwordConfirmation?.message}
+              </p>
+            )}
 
             <div>
               <input
